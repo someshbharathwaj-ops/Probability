7f7694cdd15a979594544c6d761be7ca3d119aa5
someshbharathwaj-ops <somesh.k.bharathwaj@gmail.com>
2026-03-25 04:56:48 +0530

    feat: add robots metadata route

---
+++ src/app/robots.ts
@@ -0,0 +1,14 @@
+import type { MetadataRoute } from "next";
+import { env } from "@/shared/config/env";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: `${env.appUrl}/sitemap.xml`,
+  };
+}
